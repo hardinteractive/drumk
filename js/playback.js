590b21ee8f2b86e4a166a5e3db2d6250a5465d14
Will Hardin <wrhardin@gmail.com>
2015-11-28 14:11:34 -0600

    added multi-section playback

--- js/playback.js
+++ js/playback.js
@@ -6,16 +6,16 @@ var Playback = {
 		interval: 25,
 		nextBeatTime: 0,
 		currBeat: 0,
+		currSection: 0,
 		playing: false,
 		timer: null
 	},
 	playNote: function(instrumentId){
 		var source = Playback.context.createBufferSource();
 
-		//find correct instrument
+		//find correct instrument and play its buffer.
 		for( var i = 0; i < KitManager.instruments.length; i++ ){
 			if( instrumentId === KitManager.instruments[i].id ){
-				console.log(KitManager.instruments[i]);
 				source.buffer = KitManager.instruments[i].buffer;
 				source.connect( Playback.context.destination );
 				source.start( Playback.timing.nextBeatTime );
@@ -28,20 +28,24 @@ var Playback = {
 		//advance beat by a quarter note.
 		Playback.timing.nextBeatTime += 0.25 * secondsPerBeat;
 
-		//hard-coded to 8 for now, needs to match currSection's "totalBeats"
+		//if currBeat is the last beat of the section, reset the beat counter to 0 and move to the next section.
 		if( ++Playback.timing.currBeat === 8 ){
 			Playback.timing.currBeat = 0;
+			//advance to the next section, if it's the last section then move back to first section
+			if( ++Playback.timing.currSection >= SectionManager.sections.length ){
+				Playback.timing.currSection = 0;
+			}
 		}
 	},
 	//scanner looks ahead to see if any notes/beats should be scheduled for playback
 	scanner: function(){
 		while( Playback.timing.nextBeatTime < Playback.context.currentTime + Playback.timing.lookAhead ){
-			for( var i = 0; i < SectionManager.sections[0].beatSequences.length; i++ ){
+			for( var i = 0; i < SectionManager.sections[ Playback.timing.currSection ].beatSequences.length; i++ ){
 				
-				if( SectionManager.sections[0].beatSequences[i].sequence[ Playback.timing.currBeat ].beat ){
-					console.log( SectionManager.sections[0].beatSequences[i].instrumentId );
-					Playback.playNote( SectionManager.sections[0].beatSequences[i].instrumentId );
-
+				if( SectionManager.sections[ Playback.timing.currSection ].beatSequences[i].sequence[ Playback.timing.currBeat ].beat ){
+					
+					Playback.playNote( SectionManager.sections[ Playback.timing.currSection ].beatSequences[i].instrumentId );
+					
 				}
 			}
 
@@ -58,6 +62,7 @@ var Playback = {
 			Playback.timing.playing = true;
 			Playback.timing.nextBeatTime = Playback.context.currentTime;
 			Playback.timing.currBeat = 0;
+			Playback.timing.currSection = 0;
 			Playback.scanner();
 		} else {//stop
 			Playback.timing.playing = false;
